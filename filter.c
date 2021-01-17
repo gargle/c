ef1616b97ad07aacf4413c74f1a87049ff3ca83e
Johan Laenen <johan.laenen+git@gmail.com>
2021-01-17 15:12:46 +0100

    add some comments

--- filter.c
+++ filter.c
@@ -13,6 +13,7 @@ size_t lencalc(int *array, size_t len, bool (*func)(int)) {
 }
 
 size_t filter(int *array, size_t len, int **out_array, bool (*func)(int)) {
+  /* out_array is a pointer to a pointer of ints */
   size_t newlen = lencalc(array,len,func);
   *out_array = malloc(sizeof(int)*newlen);
   int cur = 0;
@@ -34,8 +35,8 @@ bool iseven(int a) {
 
 int main(void) {
     int in[] = {1, 2, 3, 4, 5, 6};
-    int *out;
-    size_t res = filter(in, 6, &out, iseven); 
+    int *out; /* out is a pointer to integersi */
+    size_t res = filter(in, 6, &out, iseven); /* we pass the address of our pointer to filter */
     for (int i = 0; i < res; i++)
     {
         printf("%d\n", out[i]);
