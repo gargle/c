f94659220a204d2d5a447b35d8886ff662ae7cb7
Johan Laenen <johan.laenen+git@gmail.com>
2021-03-11 20:18:20 +0100

    ptr_ptr_ptr_getal

--- c.c
+++ c.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char* ps(char *s) {
-  char *t;
-  t=strdup(s);
-  puts(t);
-  return t;
-}
-
-char (*lptr[26])[7][5];
-
-char *allstars = "*****";
-char *endstars = "*   *";
-char *midstars = "  *  ";
-
-void makeH(char *sptr[7]) {
-  puts(endstars);
-  sptr++=sptr++=sptr++=endstars;
-  sptr++=allstars;
-  sptr++=sptr++=sptr=endstars;
-}
 
 void main() {
-  char *s;
-  s=ps("hello");
-  puts(s);
-  free(s);
-  makeH(lptr[7]);
+  int getal = 0;
+  int *ptr_getal;
+
+  ptr_getal = &getal;
+  printf("getal = %d\n", getal);
+  *ptr_getal = 1;
+  
+  int **ptr_ptr_getal;
+  ptr_ptr_getal = &ptr_getal;
+  **ptr_ptr_getal = 1;
+  printf("getal = %d\n", getal);
+
+  int ***ptr_ptr_ptr_getal;
+  ptr_ptr_ptr_getal = &ptr_ptr_getal;
+
+  ***ptr_ptr_ptr_getal = 2;
+  printf("getal = %d\n", getal);
 }
 
